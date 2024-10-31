39571c4e8cfd4d30e04ae5d8255c7c9102d708ee
Austin Haigh <AustinHaigh@users.noreply.github.com>
2024-10-31 14:17:40 -0700

    tmp

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sudo apt update
-sudo apt install -y git
+
+if ! git --version | grep "git version" > /dev/null; then
+	sudo apt update
+	sudo apt install -y git
+fi
+
 git clone https://github.com/bioMAPT/MAPT.git
 cd MAPT
 exec ./install.sh
