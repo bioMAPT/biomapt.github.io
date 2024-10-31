e8be0ed3562e297bb39dd8482d6a19006f759f2c
Austin Haigh <AustinHaigh@users.noreply.github.com>
2024-10-31 15:17:53 -0700

    tmp

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if ! git --version | grep "git version" > /dev/null; then
 	sudo apt install -y git
 fi
 
-git clone https://github.com/bioMAPT/MAPT.git
+git clone --filter=tree:0 --recurse-submodules https://github.com/bioMAPT/MAPT.git
 cd MAPT
 exec ./install.sh
